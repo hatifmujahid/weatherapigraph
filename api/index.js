a7ff7d25ca63f8bb8e11a4caa4aa108f585da65e
hatifmujahid <hatifmujahid08@gmail.com>
2023-06-24 17:27:12 +0500

    yes

--- api/index.js
+++ api/index.js
@@ -10,16 +10,52 @@ app.use(cors());
 const httpServer = http.createServer(app);
 
 const typeDefs = gql`
-    type Query {
-        hello: String
+    type Query{
+        getWeather(lat:Float!,lon:Float!): Weather
     }
-`;
+    type Weather{
+        weather: [WeatherInfo!]
+        main: Main!
+        wind: Wind!
+        name: String!
+        sys: Sys!
 
+    }
+    type Coord{
+        lon: Float!
+        lat: Float!
+    }
+    type WeatherInfo{
+        id: Int
+        main: String
+        description: String
+        icon: String
+    }
+    type Main{
+        temp: Float
+        feels_like: Float
+        temp_min: Float
+        temp_max: Float
+        humidity: Float
+    }
+    type Wind{
+        speed: Float
+        deg: Float
+    }
+    type Sys{
+        country: String
+    }
+
+`;
 const resolvers = {
     Query: {
-        hello: () => "Hello, world!",
-    },
+        getWeather: async(_, {lat,lon}) =>{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`);
+            return response.json();
+        },    
+    }
 };
+
 
 const startApolloServer = async (app, httpServer) => {
     const server = new ApolloServer({
